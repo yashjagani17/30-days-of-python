b3cabd012679ec2fe3d9b6335085c3b8546e9cd9
Yash Jagani <yashjagani17@gmail.com>
2026-03-26 19:39:06 +0000

    day 01: introduction

---
+++ day-01/helloworld.py
@@ -0,0 +1,41 @@
+# Level 2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print("Yash")
+print("Jagani")
+print("UK")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Yash"))
+print(type("Jagani"))
+print(type("UK"))
+
+
+# Level 3
+integer_example = 10
+float_example = 9.8
+complex_example = 4 - 4j
+string_example = "Yash Jagani"
+boolean_example = True
+list_example = [1, 2, 3, 4, 5]
+tuple_example = ("one", "two", "three")
+set_example = {"hello", "world"}
+dictionary_example = {"key": "value"}
+
+p = (2, 3)
+q = (10, 8)
+
+# euclidean distance = sqrt(diff(x)^2 + diff(y)^2)
+euclid_dist = (((p[0]-q[0])**2) + ((p[1]-q[1])**2))**0.5
+print(euclid_dist)
