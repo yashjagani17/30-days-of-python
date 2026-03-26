e06de8ce859e46c8896912e1dc667caa96eb1e41
Yash Jagani <yashjagani17@gmail.com>
2026-03-26 20:13:53 +0000

    day 02: variables, built-in functions

---
+++ day-02/variables.py
@@ -0,0 +1,51 @@
+# Level 1
+# Day 2: 30 Days of python programming
+first_name = "Yash"
+last_name = "Jagani"
+full_name = "Yash Jagani"
+country = "UK"
+city = "London"
+age = 27
+is_married = False
+is_true = True
+is_light_on = True
+day, month, year = 26, 3, 2026
+
+
+# Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(day))
+print(type(month))
+print(type(year))
+
+print(len(first_name))
+print(len(last_name) > len(first_name))
+
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_two * num_one
+divison = num_one / num_two
+remainder = num_two % num_one
+exp = num_one**num_two
+floor_division = num_one // num_two
+
+radius = 30
+area_of_circle = 3.14159265359 * (radius ** 2)
+circum_of_circle = 2 * 3.14159265359 * radius
+
+user_radius = int(input("radius: "))
+area = 3.14159265359 * (user_radius ** 2)
+
+user_first_name = input("first name: ")
+user_last_name = input("last name: ")
+user_country = input("country: ")
+user_age = int(input("age: "))
